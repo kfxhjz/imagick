8b1b4859ac1ec98ae46f2b49ad1f61181c6d35fb
hejingzheng <hejingzheng@01.ai>
2024-09-23 20:33:25 +0800

    Update GetImageBlob/GetImagesBlob to return blob when ExceptionInfo is warning kind

--- imagick/magick_wand_image.go
+++ imagick/magick_wand_image.go
@@ -1094,11 +1094,11 @@ func (mw *MagickWand) GetImageBlob() ([]byte, error) {
 	clen := C.size_t(0)
 	csblob := C.MagickGetImageBlob(mw.mw, &clen)
 	defer relinquishMemory(unsafe.Pointer(csblob))
-	if err := mw.GetLastError(); err != nil {
-		return nil, err
-	}
 	ret := C.GoBytes(unsafe.Pointer(csblob), C.int(clen))
 	runtime.KeepAlive(mw)
+	if err := mw.GetLastError(); err != nil {
+		return ret, err
+	}
 	return ret, nil
 }
 
@@ -1112,11 +1112,11 @@ func (mw *MagickWand) GetImagesBlob() ([]byte, error) {
 	clen := C.size_t(0)
 	csblob := C.MagickGetImagesBlob(mw.mw, &clen)
 	defer relinquishMemory(unsafe.Pointer(csblob))
-	if err := mw.GetLastError(); err != nil {
-		return nil, err
-	}
 	ret := C.GoBytes(unsafe.Pointer(csblob), C.int(clen))
 	runtime.KeepAlive(mw)
+	if err := mw.GetLastError(); err != nil {
+		return ret, err
+	}
 	return ret, nil
 }
 
